6b872771f76c394446927afc173b4346ceb8bfc9
= <=>
2024-12-07 22:52:59 +0700

    add file connect database

--- admin_view/pages/product_list.php
+++ admin_view/pages/product_list.php
@@ -1,17 +1,5 @@
-<?php  
-$servername = "localhost";
-$username = "root";  
-$password = "";  
-$dbname = "food_web"; 
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$sql = "SELECT * FROM products";
-$result = $conn->query($sql);
-
+<?php
+include "../config/connect_db.php"
 ?>
 <!DOCTYPE html>
 <html lang="en">
